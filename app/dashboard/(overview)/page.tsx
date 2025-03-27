1f09cd39ff56647b29553e2d6fb69f85d19fefe5
jim <279605421@qq.com>
2025-03-27 17:54:51 +0800

    Chapter 12：Mutating 数据

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,42 +7,30 @@ import {
     fetchCardData,
   } from '@/app/lib/data';
   import { Suspense } from 'react';
-  import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-
+  import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton } from '@/app/ui/skeletons';
+  import CardWrapper from '@/app/ui/dashboard/cards';
 
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         仪表板
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="已收款" value={totalPaidInvoices} type="collected" />
-        <Card title="待处理" value={totalPendingInvoices} type="pending" />
-        <Card title="总发票数" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="总客户数"
-          value={numberOfCustomers}
-          type="customers"
-        />
+
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense> 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
  
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-
-
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
